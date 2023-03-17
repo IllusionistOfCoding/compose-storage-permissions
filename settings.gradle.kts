bd4673db079903be7bde9eeff80c25cff8533c05
IllusionistOfCoding <57071410+IllusionistOfCoding@users.noreply.github.com>
2023-03-17 11:45:06 +0100

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+@Suppress("UnstableApiUsage")
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "StartupComposeTemplate"
+include(":app")
