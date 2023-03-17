f906cbaafe20ed0a251b3901cd2435acc5614ac0
pillo89 <daniele.bellini@dualb.it>
2023-03-17 12:36:12 +0100

    added Image Picker

--- app/src/main/java/com/startup/compose/template/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/startup/compose/template/ui/screen/home/HomeScreen.kt
@@ -1,18 +1,28 @@
 package com.startup.compose.template.ui.screen.home
 
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 import com.startup.compose.template.ui.route.NavigationEvent
 import com.startup.compose.template.ui.theme.StartupComposeTemplateTheme
 
@@ -50,11 +60,7 @@ fun HomeContent(
                 .fillMaxSize()
                 .background(Color.White)
         ) {
-            Text(
-                modifier = Modifier.fillMaxWidth(),
-                text = "Welcome!",
-                textAlign = TextAlign.Center
-            )
+            PhotoPickerDemoScreen()
         }
     }
 }
@@ -67,5 +73,57 @@ private fun HomeScreenPreview() {
             uiState = HomeUiState(),
             onNavigateTo = {},
         )
+    }
+}
+
+
+@Composable
+fun PhotoPickerDemoScreen() {
+    //The URI of the photo that the user has picked
+    var photoUri: Uri? by remember { mutableStateOf(null) }
+
+    //The launcher we will use for the PickVisualMedia contract.
+    //When .launch()ed, this will display the photo picker.
+    val launcher = rememberLauncherForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+        //When the user has selected a photo, its URI is returned here
+        photoUri = uri
+    }
+
+
+    Column(modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(
+            onClick = {
+                //On button press, launch the photo picker
+                launcher.launch(
+                    PickVisualMediaRequest(
+                        //Here we request only photos. Change this to .ImageAndVideo if you want videos too.
+                        //Or use .VideoOnly if you only want videos.
+                        mediaType = ActivityResultContracts.PickVisualMedia.ImageOnly,
+                    )
+                )
+            }
+        ) {
+            Text("Select Photo")
+        }
+
+        if (photoUri != null) {
+            //Use Coil to display the selected image
+            val painter = rememberAsyncImagePainter(
+                ImageRequest
+                    .Builder(LocalContext.current)
+                    .data(data = photoUri)
+                    .build()
+            )
+
+            Image(
+                painter = painter,
+                contentDescription = null,
+                modifier = Modifier
+                    .padding(5.dp)
+                    .fillMaxWidth()
+                    .border(6.0.dp, Color.Gray),
+                contentScale = ContentScale.Crop
+            )
+        }
     }
 }
